8c4061d24e4974838f6def732d052f0a156b7422
Nico Strasdat <Nico.Strasdat@tu-dresden.de>
2022-03-28 12:43:35 +0200

    Define and use subroutines

--- smo.nim
+++ smo.nim
@@ -1,12 +1,49 @@
 import std/[algorithm, sequtils, strformat, times, sugar]
 
-type Result* = object
-  a*: seq[float64]
-  b*: float64
-  steps*: int
-  time*: float64
-  violation*: float64
-  gap*: float64
+type
+  State = ref object
+    a, g, ka, dUp, dDn: seq[float64]
+    b, violation: float64
+    activeSet: seq[int]
+
+  Problem[K] = ref object
+    y: seq[float64]
+    k: K
+    lmbda: float64
+
+  Result* = object
+    a*: seq[float64]
+    b*: float64
+    steps*: int
+    time*: float64
+    violation*: float64
+    gap*: float64
+
+proc size[K](problem: Problem[K]):int {.inline.} = problem.y.len
+
+proc findMVP[K](problem: Problem[K], state: var State): (int, int) {.inline.} =
+  var
+    gmin = +Inf
+    gmax = -Inf
+    i0Idx = -1
+    j1Idx = -1
+  for lIdx, l in state.activeSet:
+    let gl = state.ka[l] - problem.y[l]
+    state.g[l] = gl
+
+    if state.dDn[l] > 0.0 and gl > gmax:
+      i0Idx = lIdx
+      gmax = gl
+    if state.dUp[l] > 0.0 and gl < gmin:
+      j1Idx = lIdx
+      gmin = gl
+  let
+    i0 = state.activeSet[i0Idx]
+    j1 = state.activeSet[j1Idx]
+  state.violation = state.g[i0] - state.g[j1]
+  state.b = -0.5 * (state.g[i0] + state.g[j1])
+  (i0Idx, j1Idx)
+
 
 proc computeDesc(kii, kij, kjj, p, tMax0, tMax1, lmbda, regParam: float64): float64 {.inline.} =
   if p <= 0.0 or tMax1 == 0.0:
@@ -15,6 +52,103 @@ proc computeDesc(kii, kij, kjj, p, tMax0, tMax1, lmbda, regParam: float64): floa
     q = kii + kjj - 2.0 * kij
     t = min(lmbda * p / max(q, regParam), min(tMax0, tMax1))
   return t * (0.5 / lmbda * q * t + p)
+
+
+proc findWS2[K](
+  problem: Problem[K],
+  i0Idx, j1Idx: int,
+  state: var State,
+  regParam: float64,
+): auto {.inline.} =
+  let
+    i0 = state.activeSet[i0Idx]
+    j1 = state.activeSet[j1Idx]
+    ki0 = problem.k.getRow(i0)
+    kj1 = problem.k.getRow(j1)
+  var
+    dmax0 = 0.0
+    dmax1 = 0.0
+    j0Idx = -1
+    i1Idx = -1
+  let
+    gi0 = state.g[i0]
+    gj1 = state.g[j1]
+    ki0i0 = ki0[i0Idx]
+    kj1j1 = kj1[j1Idx]
+    ti0Max = state.dDn[i0]
+    tj1Max = state.dUp[j1]
+  for lIdx, l in state.activeSet:
+    let
+      gl = state.g[l]
+      kll = problem.k.diag(l)
+      pi0l = gi0 - gl
+      pj1l = gl - gj1
+    if state.dUp[l] > 0.0 and pi0l > 0.0:
+      let di0l = computeDesc(
+        ki0i0, ki0[lIdx], kll,
+        pi0l, ti0Max, state.dUp[l],
+        problem.lmbda, regParam
+      )
+      if di0l > dmax0:
+        j0Idx = lIdx
+        dmax0 = di0l
+    if state.dDn[l] > 0.0 and pj1l > 0.0:
+      let dj1l = computeDesc(
+        kj1j1, kj1[lIdx], kll,
+        pj1l, tj1Max, state.dDn[l],
+        problem.lmbda, regParam
+      )
+      if dj1l > dmax1:
+        i1Idx = lIdx
+        dmax1 = dj1l
+  if dmax0 > dmax1:
+    (i0Idx, j0Idx, ki0, problem.k.getRow(state.activeSet[j0Idx]))
+  else:
+    (i1Idx, j1Idx, problem.k.getRow(state.activeSet[i1Idx]), kj1)
+
+
+proc isShrunk[K](problem: Problem[K]): bool {.inline.} = problem.k.activeSize < problem.size
+
+proc shrink[K](problem: Problem[K], state: var State, shrinkingThreshold: float64) =
+  state.activeSet = collect:
+    for l in state.activeSet:
+      let
+        glb = state.g[l] + state.b
+        glbSqr = glb * glb
+        fixUp = state.dUp[l] == 0.0 and glb < 0 and glbSqr > shrinkingThreshold * state.violation
+        fixDn = state.dDn[l] == 0.0 and glb > 0 and glbSqr > shrinkingThreshold * state.violation
+      if not (fixUp or fixDn):
+        l
+  problem.k.restrictActive(state.activeSet)
+
+proc unshrink[K](problem: Problem[K], state: var State) =
+  let n = problem.size
+  echo "Reactivate..."
+  problem.k.resetActive()
+  state.ka.fill(0.0)
+  for l in 0..<n:
+    let al = state.a[l]
+    if al != 0.0:
+      let kl = problem.k.getRow(l)
+      for r in 0..<n:
+        state.ka[r] += al / problem.lmbda * kl[r]
+  state.activeSet = (0..<n).toSeq()
+
+proc newState(y: seq[float64]): State =
+  let n = y.len
+  result = State(
+    a: newSeq[float64](n),
+    ka: newSeq[float64](n),
+    g: newSeq[float64](n),
+    dUp: newSeq[float64](n),
+    dDn: newSeq[float64](n),
+  )
+  for l in 0..<n:
+    if y[l] > 0:
+      result.dUp[l] = 1.0
+    else:
+      result.dDn[l] = 1.0
+  result.activeSet = (0..<n).toSeq()
 
 
 proc smo*[K](
@@ -29,174 +163,90 @@ proc smo*[K](
   shrinkingThreshold: float = 1.0;
 ): Result =
   # initialize
-  let
-    n = k.activeSize
-    t0 = cpuTime()
-  var
-    a = newSeq[float64](n)
-    b = 0.0
-    violation = Inf
-    ka = newSeq[float64](n)
-    g = newSeq[float64](n)
-    dUp = newSeq[float64](n)
-    dDn = newSeq[float64](n)
-    activeSet = (0..<n).toSeq()
-
-  for l in 0..<n:
-    if y[l] > 0:
-      dUp[l] = 1.0
-    else:
-      dDn[l] = 1.0
+  let t0 = cpuTime()
+  var state = newState(y)
+  let problem = Problem[K](
+    k: k,
+    y: y,
+    lmbda: lmbda,
+  )
 
   block mainPart:
     for step in 1..maxSteps:
       if shrinkingPeriod > 0 and step mod shrinkingPeriod == 0:
-        activeSet = collect:
-          for l in activeSet:
-            let
-              glb = g[l] + b
-              glbSqr = glb * glb
-              fixUp = dUp[l] == 0.0 and glb < 0 and glbSqr > shrinkingThreshold * violation
-              fixDn = dDn[l] == 0.0 and glb > 0 and glbSqr > shrinkingThreshold * violation
-            if not (fixUp or fixDn):
-              l
-        k.restrictActive(activeSet)
+        problem.shrink(state, shrinkingThreshold)
 
       # find max violation pair
-      var
-        gmin = +Inf
-        gmax = -Inf
-        i0Idx = -1
-        j1Idx = -1
-      for lIdx, l in activeSet:
-        let gl = ka[l] - y[l]
-        g[l] = gl
+      let (i0Idx, j1Idx) = problem.findMVP(state)
+      let optimal = state.violation < tolViolation
 
-        if dDn[l] > 0.0 and gl > gmax:
-          i0Idx = lIdx
-          gmax = gl
-        if dUp[l] > 0.0 and gl < gmin:
-          j1Idx = lIdx
-          gmin = gl
-
-      let
-        i0 = activeSet[i0Idx]
-        j1 = activeSet[j1Idx]
-
-      violation = g[i0] - g[j1]
-      b = -0.5 * (g[i0] + g[j1])
-      
-      let optimal = violation < tolViolation
       # print progress
       if verbose > 0 and (step mod verbose == 0 or optimal):
         var
           reg = 0.0
           lossPrimal = 0.0
           lossDual = 0.0
-        for l in 0..<n:
-          reg += ka[l] * a[l]
-          lossPrimal += max(0.0, 1.0 - y[l] * (ka[l] + b))
-          lossDual -= y[l] * a[l]
+        for l in 0..<problem.size:
+          reg += state.ka[l] * state.a[l]
+          lossPrimal += max(0.0, 1.0 - y[l] * (state.ka[l] + state.b))
+          lossDual -= y[l] * state.a[l]
         let
           objPrimal = 0.5 * reg + lossPrimal
           objDual = 0.5 * reg + lossDual
           gap = objPrimal + objDual
           dt = cpuTime() - t0
-        echo fmt"{step:10d} {dt:10.2f} {violation:10.6f} {gap:10.6f} {objPrimal:10f} {-objDual:10f} {activeSet.len:8d} of {y.len:8d}"
+        echo fmt"{step:10d} {dt:10.2f} {state.violation:10.6f} {gap:10.6f} {objPrimal:10f} {-objDual:10f} {state.activeSet.len:8d} of {y.len:8d}"
 
       # check convergence
       if optimal:
-        if k.activeSize < n:
-          echo "Reactivate..."
-          activeSet = (0..<n).toSeq()
-          k.resetActive()
-          ka.fill(0.0)
-          for l in 0..<n:
-            let al = a[l]
-            if al != 0.0:
-              let kl = k.getRow(l)
-              for r in 0..<n:
-                ka[r] += al / lmbda * kl[r]
+        if problem.isShrunk:
+          problem.unshrink(state)
           continue
         else:
           let dt = cpuTime() - t0
           result.steps = step
           result.time = dt
-          result.violation = violation
+          result.violation = state.violation
           echo fmt"done in {step} steps and {dt:.2f} seconds"
           break mainPart
 
       # determine working set
-      let (iIdx, jIdx, ki, kj) = (
+      let (iIdx, jIdx, ki, kj) = if not secondOrder:
         let
-          ki0 = k.getRow(i0)
-          kj1 = k.getRow(j1)
-        if not secondOrder:
-          (i0Idx, j1Idx, ki0, kj1)
-        else:
-          block:
-            var
-              dmax0 = 0.0
-              dmax1 = 0.0
-              j0Idx = -1
-              i1Idx = -1
-            let
-              gi0 = g[i0]
-              gj1 = g[j1]
-              ki0i0 = ki0[i0Idx]
-              kj1j1 = kj1[j1Idx]
-              ti0Max = dDn[i0]
-              tj1Max = dUp[j1]
-            for lIdx, l in activeSet:
-              let
-                gl = g[l]
-                kll = k.diag(l)
-                pi0l = gi0 - gl
-                pj1l = gl - gj1
-              if dUp[l] > 0.0 and pi0l > 0.0:
-                let di0l = computeDesc(ki0i0, ki0[lIdx], kll, pi0l, ti0Max, dUp[l], lmbda, regParam)
-                if di0l > dmax0:
-                  j0Idx = lIdx
-                  dmax0 = di0l
-              if dDn[l] > 0.0 and pj1l > 0.0:
-                let dj1l = computeDesc(kj1j1, kj1[lIdx], kll, pj1l, tj1Max, dDn[l], lmbda, regParam)
-                if dj1l > dmax1:
-                  i1Idx = lIdx
-                  dmax1 = dj1l
-            if dmax0 > dmax1:
-              (i0Idx, j0Idx, ki0, k.getRow(activeSet[j0Idx]))
-            else:
-              (i1Idx, j1Idx, k.getRow(activeSet[i1Idx]), kj1)
-      )
+          i0 = state.activeSet[i0Idx]
+          j1 = state.activeSet[j1Idx]
+        (i0Idx, j1Idx, k.getRow(i0), k.getRow(j1))
+      else:
+        problem.findWS2(i0Idx, j1Idx, state, regParam)
       
       let
-        i = activeSet[iIdx]
-        j = activeSet[jIdx]
+        i = state.activeSet[iIdx]
+        j = state.activeSet[jIdx]
 
       # find optimal step size
       let
-        pij = g[i] - g[j]
+        pij = state.g[i] - state.g[j]
         qij = ki[iIdx] + kj[jIdx] - 2.0 * ki[jIdx]
         tij = min(
           # unconstrained min
-          lmbda * pij / max(qij, regParam),
-          min(dDn[i], dUp[j])
+          problem.lmbda * pij / max(qij, regParam),
+          min(state.dDn[i], state.dUp[j])
         )
 
       # update
-      a[i] -= tij
-      dDn[i] -= tij
-      dUp[i] += tij
-      a[j] += tij
-      dDn[j] += tij
-      dUp[j] -= tij
-      let tijL = tij / lmbda
-      for lIdx, l in activeSet:
-        ka[l] += tijL * (kj[lIdx] - ki[lIdx])
+      state.a[i] -= tij
+      state.dDn[i] -= tij
+      state.dUp[i] += tij
+      state.a[j] += tij
+      state.dDn[j] += tij
+      state.dUp[j] -= tij
+      let tijL = tij / problem.lmbda
+      for lIdx, l in state.activeSet:
+        state.ka[l] += tijL * (kj[lIdx] - ki[lIdx])
 
     let dt = cpuTime() - t0
     result.steps = maxSteps
     result.time = dt
-    result.violation = violation
-  result.a = a
-  result.b = b
+    result.violation = state.violation
+  result.a = state.a
+  result.b = state.b
