233d7aa163a2815436f42fbb8b39227e3da66f7e
Nico Strasdat <Nico.Strasdat@tu-dresden.de>
2022-05-06 10:07:48 +0200

    Add parameters to python lib

--- nimsmo/nimsmo.nim
+++ nimsmo/nimsmo.nim
@@ -13,9 +13,12 @@ proc solve*(
   smoothingParam: float = 0.0;
   tolViolation: float = 1e-6;
   shift: float = 1.0;
+  verbose: int = 0;
+  shrinkingPeriod: int = 0;
 ): Result {.exportpy.} =
   let x: seq[seq[float64]] = x.to(seq[seq[float64]])
   let y: seq[float64] = y.to(seq[float64])
+  let n = x.len
 
   # define parameters
   let kernel = newGaussianKernel(x, gamma).cache(2000)
@@ -25,13 +28,14 @@ proc solve*(
   p.shift = shift
   let res = smo(
     p,
-    verbose=1000,
-    shrinkingPeriod=1000,
-    logObjective=true,
+    verbose=verbose,
+    shrinkingPeriod=if shrinkingPeriod > 0: shrinkingPeriod else: n,
+    # logObjective=true,
     tolViolation=tolViolation,
   )
-  echo fmt"It took {res.steps} steps in {res.time:.1f} seconds..."
-  echo kernel.cacheSummary()
+  if verbose > 0:
+    echo fmt"It took {res.steps} steps in {res.time:.1f} seconds..."
+    echo kernel.cacheSummary()
   res
 
 
