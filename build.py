b4477a66af9e440164d8620b14f145dc6eb15d7e
Nico Strasdat <Nico.Strasdat@tu-dresden.de>
2022-10-13 12:31:44 +0200

    Bump version

--- build.py
+++ build.py
@@ -4,7 +4,7 @@ from os.path import expanduser
 
 def build(setup_kwargs):
     """Called by poetry, the args are added to the kwargs for setup."""
-    nimbase = expanduser("~") + "/.choosenim/toolchains/nim-1.6.6/lib/nimbase.h"
+    nimbase = expanduser("~") + "/.choosenim/toolchains/nim-1.6.8/lib/nimbase.h"
     setup_kwargs.update(
         {
             "ext_modules": nythonize(
