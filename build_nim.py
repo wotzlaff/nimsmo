f082af4173a3b2617377d581a22b64cd5882271e
Nico Strasdat <Nico.Strasdat@tu-dresden.de>
2022-10-26 09:09:09 +0200

    Bump version

--- build_nim.py
+++ build_nim.py
@@ -8,6 +8,5 @@ def build(setup_kwargs):
         expanduser("~") + "/.choosenim/toolchains/nim-1.6.8/lib/nimbase.h",
         [{"name": "nimsmo", "path": "src/nimsmo/nimsmo.nim"}],
     )
-    ext_modules[0].extra_compile_args.remove('-ffast-math')
-    ext_modules[0].extra_compile_args += ['-fno-math-errno', '-funsafe-math-optimizations', '-ffinite-math-only', '-fno-rounding-math', '-fno-signaling-nans', '-fcx-limited-range', '-fexcess-precision=fast']
+    ext_modules[0].extra_compile_args.append('-fno-finite-math-only')
     setup_kwargs.update(dict(ext_modules=ext_modules))
--- nimsmo.nimble
+++ nimsmo.nimble
@@ -1,4 +1,4 @@
-version = "0.1.5"
+version = "0.1.6"
 author = "Nico Strasdat"
 description = "Nim implementation of a SMO for the training of SVMs"
 license = "no license"
