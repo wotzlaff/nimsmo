10b319d714e302f21fad979f21cb6f033361024d
Nico Strasdat <Nico.Strasdat@tu-dresden.de>
2022-10-26 07:50:37 +0200

    Try to fix fast-math

--- build_nim.py
+++ build_nim.py
@@ -4,11 +4,9 @@ from os.path import expanduser
 
 def build(setup_kwargs):
     """Called by poetry, the args are added to the kwargs for setup."""
-    nimbase = expanduser("~") + "/.choosenim/toolchains/nim-1.6.8/lib/nimbase.h"
-    setup_kwargs.update(
-        {
-            "ext_modules": nythonize(
-                nimbase, [{"name": "nimsmo", "path": "src/nimsmo/nimsmo.nim"}]
-            ),
-        }
+    ext_modules = nythonize(
+        expanduser("~") + "/.choosenim/toolchains/nim-1.6.8/lib/nimbase.h",
+        [{"name": "nimsmo", "path": "src/nimsmo/nimsmo.nim"}],
     )
+    ext_modules[0].sources.append('./src/math.c')
+    setup_kwargs.update(dict(ext_modules=ext_modules))
