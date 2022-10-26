499104386c3f19edaea66d5d7cdc1b507f44c82c
Nico Strasdat <Nico.Strasdat@tu-dresden.de>
2022-10-26 08:20:00 +0200

    Try to remove finite-math

--- build_nim.py
+++ build_nim.py
@@ -8,5 +8,6 @@ def build(setup_kwargs):
         expanduser("~") + "/.choosenim/toolchains/nim-1.6.8/lib/nimbase.h",
         [{"name": "nimsmo", "path": "src/nimsmo/nimsmo.nim"}],
     )
-    ext_modules[0].sources.append('./src/math.c')
+    ext_modules[0].extra_compile_args.remove('-ffast-math')
+    ext_modules[0].extra_compile_args += ['-fno-math-errno', '-funsafe-math-optimizations', '-ffinite-math-only', '-fno-rounding-math', '-fno-signaling-nans', '-fcx-limited-range', '-fexcess-precision=fast']
     setup_kwargs.update(dict(ext_modules=ext_modules))
--- nimsmo.nimble
+++ nimsmo.nimble
@@ -1,4 +1,4 @@
-version = "0.1.4"
+version = "0.1.5"
 author = "Nico Strasdat"
 description = "Nim implementation of a SMO for the training of SVMs"
 license = "no license"
