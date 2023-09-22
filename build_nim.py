67083bf8476b4be42352b70c2baf23cd962e0f64
Nico Strasdat <nstrasdat@gmail.com>
2023-09-22 14:20:43 +0200

    fix for new poetry version

--- build_nim.py
+++ build_nim.py
@@ -5,7 +5,7 @@ from os.path import expanduser
 def build(setup_kwargs):
     """Called by poetry, the args are added to the kwargs for setup."""
     ext_modules = nythonize(
-        expanduser("~") + "/.choosenim/toolchains/nim-1.6.8/lib/nimbase.h",
+        expanduser("~") + "/.choosenim/toolchains/nim-2.0.0/lib/nimbase.h",
         [{"name": "nimsmo", "path": "src/nimsmo/nimsmo.nim"}],
     )
     ext_modules[0].extra_compile_args.append('-fno-finite-math-only')
